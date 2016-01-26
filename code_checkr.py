09400c9d7338eebdd90019db880059bfbfd9c433
globalenigma <none@null.net>
2016-01-25 22:34:12 -0600

    Added one more thing

--- code_checkr.py
+++ code_checkr.py
@@ -3,18 +3,17 @@ import argparse
 from hashlib import md5
 
 def main():
-    if args.file:
-        hash_it(args.file)
+    if args.infile:
+        hash_it(args.infile)
 
-def hash_it(file_name):
-    with open(file_name, mode='rb') as content:
+def hash_it(in_file):
+    with open(in_file, mode='rb') as content:
         for line in content:
-            print(md5(line).hexdigest()[-3:] + ': ' + line.decode('utf-8').strip('\n'))
-        print('\nFull md5 hash of: ' + file_name + ' - ' + md5(open(file_name, 'rb').read()).hexdigest())
+            print(md5(line.rstrip()).hexdigest()[-3:] + ': ' + line.decode('utf-8').rstrip())
+        print('\nFull md5 hash of: ' + in_file + ' - ' + md5(open(in_file, 'rb').read()).hexdigest())
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Simple little code checkr')
-    parser.add_argument('-f', '--file', required=True, help='file to do hashy things with')
+    parser.add_argument('-i', '--infile', required=True, help='input file to do hashy things with')
     args = parser.parse_args()
-    print(args.file)
     main()
